5db20ac4ddf7608080393f1d66ee6de20559f6e0
duxin <1102003453@qq.com>
2021-02-21 22:36:19 +0800

    [modify]添加常用方法

--- client/MediaStream/index.js
+++ client/MediaStream/index.js
@@ -1,26 +1,63 @@
 import React, { useEffect, useRef } from "react";
 import "./index.less";
 
-function MediaStream() {
+const mixedStream = new MediaStream();
+
+function Index() {
 	const videoRef = useRef(null);
+	const rvideoRef = useRef(null);
+	const recorderRef = useRef(null);
 
 	useEffect(() => {
-		navigator.mediaDevices.getUserMedia =
-			navigator.mediaDevices.getUserMedia ||
-			navigator.mediaDevices.webkitGetUserMedia ||
-			navigator.mediaDevices.mozGetUserMedia ||
-			navigator.mediaDevices.msGetUserMedia;
+		//获取媒体设备
+		navigator.mediaDevices.enumerateDevices().then(res => {
+			console.log(res);
+		});
 
-      navigator.mediaDevices.getUserMedia({ video: true, audio: true }).then(stream => {
+		//获取用户媒体流 video、audio可传入deviceInfo
+		navigator.mediaDevices.getUserMedia({ video: true, audio: true }).then(stream => {
+			//录制屏幕
+			// navigator.mediaDevices.getDisplayMedia().then(stream => {
 			videoRef.current.srcObject = stream;
+			const videoTrack = stream.getVideoTracks();
+			const audioTrack = stream.getAudioTracks();
+
+			console.log("video track -----", videoTrack);
+			console.log("audio track -----", audioTrack);
+
+			videoTrack.forEach(track => mixedStream.addTrack(track));
+			audioTrack.forEach(track => mixedStream.addTrack(track));
+			console.log("combine track", mixedStream);
+
+			recorderRef.current = new MediaRecorder(stream, { mineType: "video/webm;codecs=h264" });
+
+			recorderRef.current.ondataavailable = e => {
+				console.log("recorder -----", e);
+				rvideoRef.current.src = URL.createObjectURL(e.data);
+			};
+
+			recorderRef.current.start();
+
+			setTimeout(() => {
+				recorderRef.current.pause();
+			}, 1000);
+
+			setTimeout(() => {
+				recorderRef.current.resume();
+			}, 2000);
+
+			setTimeout(() => {
+				recorderRef.current.stop();
+			}, 3000);
 		});
 	}, []);
 
 	return (
 		<div className="media-stream">
 			<video autoPlay={true} muted ref={videoRef} controls></video>
+			<video ref={rvideoRef} controls></video>
 		</div>
 	);
 }
 
-export default MediaStream;
+export default Index;
